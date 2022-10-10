2c27084c6d0fb8ba7093f0321963a238d9bb6aee
waylandsingh <wmsingh@ucdavis.edu>
2022-10-09 21:08:11 -0700

    navbar element created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import styles from './style';
 
-const App = () => { 
-  return (
-    <div>App</div>
-  )
-}
+
+
+import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero } from './components'
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className = {`${styles.padding} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />sfsdfsss
+      </div>
+    </div>
+
+  {/* hero section */}
+  <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Hero /> 
+    </div>
+  </div>
+
+    {/* hero section */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Stats />
+      <Business /> 
+      <Billing /> 
+      <CardDeal />
+      <Testimonials />
+      <Clients />
+      <CTA />  
+      <Footer /> 
+    </div>
+  </div>
+
+  
+
+  </div>
+)
+
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Feedback from "./Feedback";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+// helper file to allow importing from the entire folder at once 
+
+export {
+    Navbar, 
+    Billing, 
+    Clients,
+    CardDeal,
+    Business,
+    CTA,
+    Hero, 
+    Stats,
+    Testimonials,
+    Footer
+}
